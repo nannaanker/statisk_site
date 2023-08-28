fe958c008433dc2411321e1ddda32655b49ebcdb
Nanna <naho0001@stud.kea.dk>
2023-08-28 13:18:39 +0200

    first commit

---
+++ generel.js
@@ -0,0 +1,21 @@
+fetch("https://kea-alt-del.dk/t7/api/products")
+.then(res=>res.json())
+.then(showProduct)
+
+function showHProduct(products){
+    products.forEach(showroducts)
+}
+
+function showProduct(product){
+    const template = document.querySelector(".produkt_kolonne").content;
+    const copy =  produkt_kolonne.cloneNode(true); 
+    copy.querySelector(".navn").textContent = product.brandname;
+
+    copy.querySelector(".alias").textContent = hero.alias;
+    copy.querySelector(".origin").textContent = hero.origin;
+    copy.querySelector(".måned").textContent = hero.birthday.month;
+    copy.querySelector(".dag").textContent = hero.birthday.day;
+    copy.querySelector("img").src = hero.image;
+    const parent = document.querySelector(".grid");
+    parent.appendChild(copy);
+}
