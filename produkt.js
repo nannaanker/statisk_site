75465e12d7859c2665a5925d096eb08c37337311
Nanna <naho0001@stud.kea.dk>
2023-08-29 15:17:18 +0200

    java

---
+++ produkt.js
@@ -0,0 +1,15 @@
+//https://kea-alt-del.dk/t7/api/products/1525
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+
+fetch("https://kea-alt-del.dk/t7/api/products/" + id)
+.then((response)=> response.json())
+.then((data) => showProduct(data));
+
+function showProduct(product) {
+    console.log(product);
+    document.querySelector(".produkt-oplysninger h2").textContent = product.productdisplayname;
+    document.querySelector(".produkt-oplysninger h1").textContent = product.brandname;
+    document.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+}
+
---
+++ produktliste.js
@@ -0,0 +1,38 @@
+fetch("https://kea-alt-del.dk/t7/api/products?limit=10")
+.then(res=>res.json())
+.then(showProducts)
+
+function showProducts(products){
+    products.forEach(showProduct)
+    
+}
+
+function showProduct(product){
+    const template = document.querySelector("template").content;
+    const copy =  template.cloneNode(true); 
+   
+   //find brandname//
+    copy.querySelector(".navn").textContent = product.brandname;
+   
+   //vis hvis udsolgt//
+    if(product.soldout){ 
+        copy.querySelector(".udsolgt").classList.add("udsolgt_overlay")
+    }
+    //vis pris//
+    copy.querySelector(".pris").textContent = product.price;
+    //vis discount//
+    copy.querySelector("span").textContent = product.discount;
+    if(product.discount > 0){
+        copy.querySelector("span").textContent = product.discount; 
+    }
+
+    else{copy.querySelector(".discount_info").remove()};
+   
+   //hent id info til produkt side//
+
+    copy.querySelector(".read-more").setAttribute("href", `produkt.html?id=${product.id}`);
+
+    //sæt det ind// 
+    const parent = document.querySelector(".produkt");
+    parent.appendChild(copy);
+}
